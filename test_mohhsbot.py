cf5f7ff0d06d9617cbb64121212c3ae24ee39ea7
Martin Uribe <clamytoe@users.noreply.github.com>
2017-11-24 20:48:01 -0600

    Had to remove some tests because those were for functionality that were not part of this bot.

--- test_mohhsbot.py
+++ test_mohhsbot.py
@@ -17,21 +17,3 @@ def test_slack_connection():
 def test_bot_id():
     assert mohhsbot.AT_BOT == f'<@{BOT_ID}>'
 
-
-def test_save_keywords():
-    assert 'archive' in mohhsbot.STORE_COMMANDS
-    assert 'backup' in mohhsbot.STORE_COMMANDS
-    assert 'keep' in mohhsbot.STORE_COMMANDS
-    assert 'put' in mohhsbot.STORE_COMMANDS
-    assert 'remember' in mohhsbot.STORE_COMMANDS
-    assert 'save' in mohhsbot.STORE_COMMANDS
-    assert 'store' in mohhsbot.STORE_COMMANDS
-
-
-def test_retrieve_keywords():
-    assert 'find' in mohhsbot.RETRIEVE_COMMANDS
-    assert 'get' in mohhsbot.RETRIEVE_COMMANDS
-    assert 'give' in mohhsbot.RETRIEVE_COMMANDS
-    assert 'lookup' in mohhsbot.RETRIEVE_COMMANDS
-    assert 'retrieve' in mohhsbot.RETRIEVE_COMMANDS
-    assert 'search' in mohhsbot.RETRIEVE_COMMANDS
